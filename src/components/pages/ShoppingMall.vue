fa48138e39c255ec39283c64894be73d62416616
wey <149201216@qq.com>
2019-03-10 19:05:08 +0800

    package floor into component

--- src/components/pages/ShoppingMall.vue
+++ src/components/pages/ShoppingMall.vue
@@ -49,26 +49,7 @@
       </div>
     </div>
     <!-- 楼层 -->
-    <div class="floor">
-      <div class="floor-irregular">
-        <div class="floor-one">
-          <img :src="floor1_0.image" width="100%" />
-        </div>
-        <div>
-          <div class="floor-two">
-            <img :src="floor1_1.image" width="100%" />
-          </div>
-          <div>
-            <img :src="floor1_2.image" width="100%" />
-          </div>
-        </div>
-      </div>
-    </div>
-    <div class="floor-regular">
-      <div v-for="(item, index) in floor1.slice(1)" :key="index">
-        <img :src="item.image" width="100%" />
-      </div>
-    </div>
+    <floor :floorData="floor1"></floor>
 
   </div>
 </template>
@@ -77,6 +58,8 @@
   import axios from 'axios'
   import { swiper, swiperSlide } from 'vue-awesome-swiper'
   import 'swiper/dist/css/swiper.css'
+  import floor from '../../components/common/floor.vue'
+
   export default {
     data() {
       return {
@@ -94,10 +77,7 @@
         floor1_2: {}
       }
     },
-    components: {
-      swiper,
-      swiperSlide
-    },
+    components: {swiper, swiperSlide, floor},
     created() {
       axios({
         url: 'https://www.easy-mock.com/mock/5c838bf20994c30d33e411ca/weymall/index',
@@ -117,9 +97,6 @@
           this.recommendGoods = response.data.data.recommend;
           // 1楼
           this.floor1 = response.data.data.floor1
-          this.floor1_0 = this.floor1[0];
-          this.floor1_1 = this.floor1[1];
-          this.floor1_2 = this.floor1[2];
         }
       })
       .catch(error => {
@@ -182,42 +159,5 @@
     font-size: 12px;
     color: #333;
     border-bottom: 1px solid #eee;
-  }
-  .recommand-item {
-    width: 99%;
-    border-right: 1px solid #eee;
-    text-align: center;
-  }
-  .floor-irregular {
-    display: flex;
-    flex-direction: row;
-    background-color: #fff;
-    border-bottom: 1px solid #ddd;
-  }
-  .floor-irregular div {
-    width: 10rem;
-    box-sizing: border-box;
-    -webkit-box-sizing: border-box;
-  }
-  .floor-one {
-    border-right: 1px solid #ddd;
-  }
-  .floor-two {
-    border-bottom: 1px solid #ddd;
-  }
-  .floor-regular {
-    display: flex;
-    flex-direction: row;
-    flex-wrap: wrap;
-    background-color: #fff;
-  }
-  .floor-regular div {
-    -webkit-box-sizing: border-box;
-    box-sizing: border-box;
-    width: 10rem;
-    border-bottom: 1px solid #ddd;
-  }
-  .floor-regular div:nth-child(odd) {
-    border-right: 1px solid #ddd;
   }
 </style>
