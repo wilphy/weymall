9609c4ae52eb53335859abd457e8474daf7c70af
wey <149201216@qq.com>
2019-03-13 18:52:49 +0800

    Import json data into the database

--- server/Controller/goods.js
+++ server/Controller/goods.js
@@ -4,6 +4,7 @@ let router = new Router()
 const mongoose = require('mongoose')
 const fs = require('fs')
 
+//商品详情
 router.get('/insertAllGoodsInfo',async(ctx) => {
   fs.readFile('./newGoods.json','utf8',(err,data) => {
     data=JSON.parse(data)
@@ -21,6 +22,47 @@ router.get('/insertAllGoodsInfo',async(ctx) => {
     })
   })
   ctx.body = "开始导入数据"
+})
+
+//商品分类
+router.get('/insertAllCategory',async(ctx)=>{
+  fs.readFile('./category.json','utf8',(err,data)=>{
+      data=JSON.parse(data)
+      let saveCount=0
+      const Category = mongoose.model('Category')
+      data.RECORDS.map((value,index)=>{
+          console.log(value)
+          let newCategory = new Category(value)
+          newCategory.save().then(()=>{
+              saveCount++
+              console.log('成功'+saveCount)
+          }).catch(error=>{
+               console.log('失败：'+error)
+          })
+      })
+  })
+  ctx.body="开始导入数据"
+})
+
+//商品子类
+router.get('/insertAllCategorySub',async(ctx)=>{
+  fs.readFile('./category_sub.json','utf8',(err,data)=>{
+      data = JSON.parse(data)
+      let saveCount = 0 
+      const CategorySub = mongoose.model('CategorySub')
+      data.RECORDS.map((value,index)=>{
+          console.log(value)
+          let newCategorySub = new CategorySub(value)
+          newCategorySub.save().then(()=>{
+              saveCount++
+              console.log('成功插入'+saveCount)
+              
+          }).catch(error=>{
+              console.log('插入失败:'+error)
+          })
+      }) 
+  })
+  ctx.body="开始导入数据"
 })
 
 module.exports = router
--- src/router/index.js
+++ src/router/index.js
@@ -3,18 +3,13 @@ import Router from 'vue-router'
 import ShoppingMall from '@/components/pages/ShoppingMall'
 import Register from '@/components/pages/Register'
 import Login from '@/components/pages/Login'
-import Nav from '@/components/pages/Nav'
 
 Vue.use(Router)
 Vue.use(Login)
 
 export default new Router({
   routes: [
-    {path: '/nav',name: 'Nav',component: Nav,
-      children:[
-        {path: '/',name: 'ShoppingMall',component: ShoppingMall},
-      ]
-    },
+    {path: '/',name: 'ShoppingMall',component: ShoppingMall},
     {path: '/register', name: 'Register', component: Register},
     {path: '/login', name: 'Login', component: Login},
   ]
